311ad184c52826623ac5091000a2eac3a1ce39f7
forest <kazushima1702@gmail.com>
2025-02-01 14:50:26 +0900

    route

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,6 +1,5 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
-import { Post } from "@prisma/client";
 
 type RouteParams = {
   params: {
@@ -8,79 +7,73 @@ type RouteParams = {
   };
 };
 
-type RequestBody = {
-  title: string;
-  content: string;
-  coverImageURL: string;
-  categoryIds: string[];
-};
-
-export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
+// GETリクエスト: 特定のToDoアイテムを取得
+export const GET = async (req: NextRequest, routeParams: RouteParams) => {
   try {
     const id = routeParams.params.id;
-    const requestBody: RequestBody = await req.json();
-
-    // 分割代入
-    const { title, content, coverImageURL, categoryIds } = requestBody;
-
-    // categoryIds に該当するカテゴリが存在するか確認
-    const categories = await prisma.category.findMany({
-      where: {
-        id: {
-          in: categoryIds,
-        },
-      },
-    });
-    if (categories.length !== categoryIds.length) {
-      throw new Error("指定されたカテゴリが存在しません");
-    }
-
-    // 中間テーブルのレコードを削除
-    await prisma.postCategory.deleteMany({
-      where: { postId: id },
-    });
-
-    // 投稿記事テーブルにレコードを追加
-    const post: Post = await prisma.post.update({
+    const todo = await prisma.toDo.findUnique({
       where: { id },
-      data: {
-        title, // title: title の省略形であることに注意。以下も同様
-        content,
-        coverImageURL,
+      include: {
+        category: true, // カテゴリを含めて取得
       },
     });
-
-    // 中間テーブルにレコードを追加
-    for (const categoryId of categoryIds) {
-      await prisma.postCategory.create({
-        data: {
-          postId: post.id,
-          categoryId: categoryId,
-        },
-      });
+    if (!todo) {
+      return NextResponse.json(
+        { error: "ToDoが見つかりません" },
+        { status: 404 }
+      );
     }
-
-    return NextResponse.json(post);
+    return NextResponse.json(todo);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "投稿記事の変更に失敗しました" },
+      { error: "データの取得に失敗しました" },
       { status: 500 }
     );
   }
 };
 
-export const DELETE = async (req: NextRequest, routeParams: RouteParams) => {
+// PUTリクエスト: 特定のToDoアイテムを更新
+export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
+  const { id } = routeParams.params;
+  const { title, description, dueDate, priority, completed, category } =
+    await req.json();
   try {
-    const id = routeParams.params.id;
-    const post: Post = await prisma.post.delete({
+    const updatedToDo = await prisma.toDo.update({
       where: { id },
+      data: {
+        title,
+        description,
+        dueDate: new Date(dueDate), // ここで日付を正しい形式に変換
+        priority,
+        completed,
+        category: {
+          connect: category.map((cat: { id: string }) => ({ id: cat.id })),
+        },
+      },
     });
-    return NextResponse.json({ msg: `「${post.title}」を削除しました。` });
+    return NextResponse.json(updatedToDo);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "投稿記事の削除に失敗しました" },
+      { error: "ToDoの更新に失敗しました" },
+      { status: 500 }
+    );
+  }
+};
+
+// DELETEリクエスト: 特定のToDoアイテムを削除
+export const DELETE = async (req: NextRequest, routeParams: RouteParams) => {
+  const { id } = routeParams.params;
+  try {
+    await prisma.toDo.delete({
+      where: { id },
+    });
+    return NextResponse.json({ message: "ToDoの削除に成功しました" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "ToDoの削除に失敗しました" },
       { status: 500 }
     );
   }
