ed4b87e498960188374439cf7a1bb59c5aa7cba2
forest <kazushima1702@gmail.com>
2025-02-01 14:28:37 +0900

    app

--- src/app/_types/ToDo.ts
+++ src/app/_types/ToDo.ts
@@ -1,18 +1,12 @@
 import { Category } from "./Category";
 import { CoverImage } from "./CoverImage";
 
-export interface ToDo {
+export type ToDo = {
   id: string;
-
   title: string;
-
   description: string;
-
   dueDate: string;
-
   priority: "low" | "medium" | "high";
-
   completed: boolean;
-
-  category: { id: string; name: string }[];
-}
+  category?: { id: string; name: string }[];
+};
